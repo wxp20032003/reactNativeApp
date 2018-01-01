bf6fcdd74b55c368d0d15be11af87ffba5cc9697
wxp20032003 <2353010324@qq.com>
2018-01-02 00:00:02 +0800

    增加图片logo

--- firstApp/App.js
+++ firstApp/App.js
@@ -8,13 +8,14 @@ import {
   Alert,
   StatusBar,
   TouchableHighlight,
-  Dimensions
+  Dimensions,
+  Image
 
 } from 'react-native';
 import Detail from './Detail';
 
 import { StackNavigator } from 'react-navigation';
-var {height, width} = Dimensions.get('window');
+var { height, width } = Dimensions.get('window');
 
 class HomeScreen extends React.Component {
   static navigationOptions = { header: null };
@@ -22,6 +23,10 @@ class HomeScreen extends React.Component {
     const { navigate } = this.props.navigation;
     return (
       <View style={styles.container}>
+        <View style={styles.avatarview}>
+          <Image style={styles.avatarimage} source={require('./nsImage/nsbdlogo.png')}>
+          </Image>
+        </View>
         {/* <StatusBar hidden={false} translucent={true} backgroundColor={"#1FB9FF"} /> */}
         <View style={styles.inputview}>
           <TextInput style={styles.textinput} placeholder="请输入用户名" />
@@ -40,24 +45,34 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#f4f4f4',
-
     alignItems: 'center',
     justifyContent: 'center'
   },
+  avatarview: {//登录图标区域  
+    height: 150,
+    backgroundColor: '#ECEDF1',
+    justifyContent: 'center',
+  },
+  avatarimage: {//登录图标  
+    width: 100,
+    height: 100,
+    alignSelf: 'center'
+  },
   inputview: {//用户名/密码区域  
     height: 100,
+    
   },
   textinput: {//用户名/密码输入框  
     flex: 1,
     borderWidth: 0,
     fontSize: 16,
-    width:width-40,
+    width: width - 40,
   },
   buttonBackground: {
     backgroundColor: '#5a98de',
     borderRadius: 5,
     padding: 10,
-    width:width-40,
+    width: width - 40,
   },
   buttonText: {
     textAlign: 'center',
