a38267851339cacbe7fff53c90b4bcb778b68b54
wxp20032003 <2353010324@qq.com>
2018-01-01 23:47:00 +0800

    输入框改变

--- firstApp/App.js
+++ firstApp/App.js
@@ -7,12 +7,15 @@ import {
   TextInput,
   Alert,
   StatusBar,
-  TouchableHighlight
+  TouchableHighlight,
+  Dimensions
 
 } from 'react-native';
 import Detail from './Detail';
 
 import { StackNavigator } from 'react-navigation';
+var {height, width} = Dimensions.get('window');
+
 class HomeScreen extends React.Component {
   static navigationOptions = { header: null };
   render() {
@@ -25,8 +28,8 @@ class HomeScreen extends React.Component {
           <TextInput style={styles.textinput} placeholder="请输入密码" secureTextEntry={true} />
         </View>
 
-        <TouchableHighlight  onPress={() => navigate('Detail')} style={styles.buttonBackground}>
-         <Text style={styles.buttonText}>登录</Text>
+        <TouchableHighlight onPress={() => navigate('Detail')} style={styles.buttonBackground}>
+          <Text style={styles.buttonText}>登录</Text>
         </TouchableHighlight>
       </View>
     );
@@ -36,30 +39,30 @@ class HomeScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f4f4f4',
 
     alignItems: 'center',
     justifyContent: 'center'
   },
   inputview: {//用户名/密码区域  
     height: 100,
-    width: 200,
   },
   textinput: {//用户名/密码输入框  
     flex: 1,
     borderWidth: 0,
     fontSize: 16,
+    width:width-40,
   },
   buttonBackground: {
     backgroundColor: '#5a98de',
-    borderRadius:5,
-    padding:10,
-    width: 200,
+    borderRadius: 5,
+    padding: 10,
+    width:width-40,
   },
-  buttonText:{
-    textAlign:'center',
-    justifyContent:'center',
-    color:'#fff'
+  buttonText: {
+    textAlign: 'center',
+    justifyContent: 'center',
+    color: '#fff'
   }
 });
 const SimpleApp = StackNavigator({
